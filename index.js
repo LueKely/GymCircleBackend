fbede19d9dad5f4ab4c4d0378a51666563d550e1
Anunciacion Lue <luekely43110@gmail.com>
2023-05-20 22:19:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+app.use(express.static('public'));
 app.get('/', (req, res) => {
 	res.send('Hello World!');
+});
+
+const joe = '/tite';
+
+app.get(joe, (req, res) => {
+	res.send('tite!');
 });
 
 app.listen(port, () => {
