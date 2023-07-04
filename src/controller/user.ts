868d0bb9662a367e871cde29c96c92d7328ecdf6
LueKely <luekely43110@gmail.com>
2023-07-04 12:32:50 +0800

    FIX: date and time

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -9,13 +9,21 @@ export default {
 	async generateTransaction(req: Request, res: Response) {
 		const userId = req.body.payload;
 
+		function getCurrentDate() {
+			const today = new Date();
+			const year = today.getFullYear();
+			const month = String(today.getMonth() + 1).padStart(2, '0');
+			const day = String(today.getDate()).padStart(2, '0');
+			return `${year}-${month}-${day}`;
+		}
+
 		const transactionInfo: Transaction = {
 			id: crypto.randomUUID(),
 			transactionName: req.body.name,
 			type: req.body.type,
 			buyerId: userId,
 			price: req.body.price,
-			date: new Date().toLocaleDateString('en-US'),
+			date: getCurrentDate(),
 			status: 'not paid',
 		};
 
@@ -44,7 +52,7 @@ export default {
 	async getInfo(req: Request, res: Response, next: NextFunction) {
 		const userId = req.body.payload;
 		const data = await sqlExe(
-			'SELECT user.user_id,user.name,user.email,user.age,user.address,user.tier,attendance.* FROM user JOIN attendance ON user.user_id = attendance.user_id WHERE user.user_id=?',
+			'SELECT user.user_id,user.points,user.name,user.email,user.age,user.address,user.tier,attendance.* FROM user JOIN attendance ON user.user_id = attendance.user_id WHERE user.user_id=?',
 			[userId]
 		);
 		res.send(data[0]).status(200);
